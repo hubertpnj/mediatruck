43a72b2d7e1c62ec6e16ad02a2e4237132e6a629
Hubert Janoszka <HubertPNJ@protonmail.com>
2024-12-09 17:06:49 +0100

    Add initializr generated project

---
+++ src/main/java/hpnj/mediatruck/MediatruckApplication.java
@@ -0,0 +1,13 @@
+package hpnj.mediatruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediatruckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hpnj/mediatruck/MediatruckApplicationTests.java
@@ -0,0 +1,13 @@
+package hpnj.mediatruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatruckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
